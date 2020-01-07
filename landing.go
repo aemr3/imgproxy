0ca8c40a6385841be7fb34ba1f289f2c50617697
Emre <aemr3@users.noreply.github.com>
2020-01-07 12:04:13 +0300

    Update landing.go

--- landing.go
+++ landing.go
@@ -5,11 +5,8 @@ import "net/http"
 var landingTmpl = []byte(`
 <!doctype html>
 <html>
-	<head><title>Hey, I'm imgproxy!</title></head>
-	<body>
-		<h1>Hey, I'm imgproxy!</h1>
-		<p style="font-size:1.2em">You can get me here: <a href="https://github.com/imgproxy/imgproxy" target="_blank">https://github.com/imgproxy/imgproxy</a></p>
-	</body>
+<head><title>.</title></head>
+<body>.</body>
 </html>
 `)
 
